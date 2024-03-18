45fe8aa1674a16ef1f67737d78f8c2e5327100d4
Francisco Diaz Roldan <francisco.diazro@viewnet.com>
2024-03-18 11:28:18 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Francisco Díaz Roldán on 18/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
